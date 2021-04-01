9c98c4a34cd6c3a952783cc7c6abde64ecd92454
Arzanish Yusuf Nadeem <arzanish.y.n@outlook.com>
2021-04-01 17:47:18 +0530

    made minor changes

--- 10-String/Pattern_Searching/Naive.java
+++ 10-String/Pattern_Searching/Naive.java
@@ -1,10 +1,11 @@
 package Pattern_Searching;
 
+// Time Complexity O((n-m+1)*m)
 public class Naive {
 
   public static void naive(String txt, String pat) {
     int m = pat.length();
-    int n = pat.length();
+    int n = txt.length();
     for (int i = 0; i <= n - m; i++) {
       int j;
       for (j = 0; j < m; j++) {
