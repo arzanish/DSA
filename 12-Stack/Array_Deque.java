fac10725000f2593a5d5c7870ac95bf98f6fcc0d
Arzanish Yusuf Nadeem <arzanish.y.n@outlook.com>
2021-04-05 18:15:41 +0530

    Implemented Stack Using Array Deque

---
+++ 12-Stack/Array_Deque.java
@@ -0,0 +1,17 @@
+import java.util.ArrayDeque;
+
+public class Array_Deque {
+  public static void main(String[] args) {
+
+    ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    System.out.println(stack.peek());
+    System.out.println(stack.pop());
+    System.out.println(stack.peek());
+
+  }
+}
