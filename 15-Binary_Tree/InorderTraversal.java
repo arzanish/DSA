f1747d0bbd9d5d0a012289a97c0b1fdf56ae69a8
Arzanish Yusuf Nadeem <arzanish.y.n@outlook.com>
2021-05-03 17:20:46 +0530

    Implemented Inorder Traversal of Binary Tree

---
+++ 15-Binary_Tree/InorderTraversal.java
@@ -0,0 +1,23 @@
+
+public class InorderTraversal {
+
+  public static void main(String[] agrs) {
+    Node root = new Node(10);
+    root.left = new Node(20);
+    root.right = new Node(30);
+    root.right.left = new Node(40);
+    root.right.right = new Node(50);
+
+    inorder(root);
+
+  }
+
+  static void inorder(Node root) {
+    if (root != null) {
+      inorder(root.left);
+      System.out.print(root.key + " ");
+      inorder(root.right);
+    }
+  }
+
+}
