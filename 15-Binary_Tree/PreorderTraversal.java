e3052445a899a922680b6e42026d4d86c72e44f2
Arzanish Yusuf Nadeem <arzanish.y.n@outlook.com>
2021-05-03 17:24:26 +0530

    Implemented Preorder Traversal in Binary Tree

---
+++ 15-Binary_Tree/PreorderTraversal.java
@@ -0,0 +1,20 @@
+public class PreorderTraversal {
+
+  public static void main(String[] args) {
+    Node root = new Node(10);
+    root.left = new Node(20);
+    root.right = new Node(30);
+    root.right.left = new Node(40);
+    root.right.right = new Node(50);
+
+    preorder(root);
+  }
+
+  static void preorder(Node root) {
+    if (root != null) {
+      System.out.print(root.key + " ");
+      preorder(root.left);
+      preorder(root.right);
+    }
+  }
+}
