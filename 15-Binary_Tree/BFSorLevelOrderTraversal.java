2393e81b1ab19b9d68e25c2e5cb612cb8e2d1254
Arzanish Yusuf Nadeem <arzanish.y.n@outlook.com>
2021-05-04 18:02:11 +0530

    Implemented Level Order Traversal / Breadth First Search

---
+++ 15-Binary_Tree/BFSorLevelOrderTraversal.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFSorLevelOrderTraversal {
+
+  public static void main(String[] args) {
+    Node root = new Node(10);
+    root.left = new Node(20);
+    root.right = new Node(30);
+    root.left.left = new Node(40);
+    root.left.right = new Node(50);
+    root.right.left = new Node(60);
+    root.right.right = new Node(70);
+
+    bfs(root);
+  }
+
+  static void bfs(Node root) {
+    if (root == null)
+      return;
+    Queue<Node> q = new LinkedList<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+      Node curr = q.poll();
+      System.out.print(curr.key + " ");
+      if (curr.left != null)
+        q.add(curr.left);
+      if (curr.right != null)
+        q.add(curr.right);
+
+    }
+  }
+}
