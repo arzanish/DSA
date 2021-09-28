e6ddc34ba15429ae74d45fe19fcc317796794764
Arzanish Yusuf Nadeem <arzanish.y.n@outlook.com>
2021-09-28 16:51:03 +0530

    .

--- 18-Graph/BFS_version1.java
+++ 18-Graph/BFS_version1.java
@@ -40,6 +40,23 @@ public class BFS_version1 {
 
   public static void main(String[] ags) {
 
+    int V = 5;
+    ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
+
+    for (int i = 0; i < V; i++)
+      adj.add(new ArrayList<Integer>());
+
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 3, 4);
+    addEdge(adj, 2, 4);
+
+    System.out.println("Following is Breadth First Traversal: ");
+    BFS_v1(adj, V, 0);
+
   }
 
 }
